f7b782471793ef344348865c7a4f0c5d25818453
Daisy Guerrero <dguerrero1@bsd220.org>
2022-04-05 11:01:03 -0500

    Created fishing pole object and reset game function

--- fishfish/GameScene.swift
+++ fishfish/GameScene.swift
@@ -9,12 +9,24 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    private var fish = 1
+    var fish = 1
+    var fishingPole = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        resetGame()
+    }
+    
+    func makeFishingPole() {
+        fishingPole = SKSpriteNode(color: .white, size: CGSize(width: 10, height: frame.height/4))
+        fishingPole.position = CGPoint(x: frame.midX, y: frame.minY + 1250)
+        fishingPole.name = "fishing pole"
+        fishingPole.physicsBody = SKPhysicsBody(rectangleOf: fishingPole.size)
+        fishingPole.physicsBody?.isDynamic = false
+        addChild(fishingPole)
+    }
+    
+    func resetGame() {
+        makeFishingPole()
     }
 }
 
